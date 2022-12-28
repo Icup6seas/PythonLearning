f12e5f229bf55af48d520413c9ebed4428039ec6
Chris <chris.cosnowski@gmail.com>
2022-12-28 15:50:47 -0500

    Saving

--- Assessment1.py
+++ Assessment1.py
@@ -77,7 +77,6 @@ d = {'k1':[1,2,{'k2':['this is tricky',{'tough':[1,2,['hello']]}]}]}
 print(d['k1'][2]['k2'][1]['tough'][2][0])
 
 
-
 # Use a set to find the unique values of the list below:
 list5 = [1,2,2,33,4,4,11,22,3,3,2]
 setList = set(list5)
