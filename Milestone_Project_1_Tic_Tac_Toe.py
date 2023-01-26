29b8f4142af17d4e4c60c0b6f654a94397dbc54f
Chris <chris.cosnowski@gmail.com>
2023-01-26 16:45:31 -0500

    Updated from work pc

--- Milestone_Project_1_Tic_Tac_Toe.py
+++ Milestone_Project_1_Tic_Tac_Toe.py
@@ -18,7 +18,7 @@ def welcome_screen():
     # Validation
     while quit_game == False:
         while result.isdigit() == False or within_range == False:
-            result = input("Enter 1 to START or ANY OTHER NUMBER to Quit: ")
+            result = input("Enter 1 to START or 2 to Quit: ")
             # Check if digit
             if result.isdigit() == False:
                 # clear()
@@ -27,11 +27,10 @@ def welcome_screen():
             elif result.isdigit() == True and int(result) in acceptable_result_range_list:
                 if int(result) == 1:
                     # Check for option 1
-                    # clear()
                     within_range = True
                     display()
                 elif int(result) == 2:
-                    # clear()
+                    clear()
                     print("Thanks for playing!")
                     quit_game = True
                     quit()
@@ -41,7 +40,8 @@ def welcome_screen():
 
 # Clear display
 def clear():
-    os.system('cls')
+    # os.system('cls')
+    print('\n' * 100)
 
 
 # Displays the board
@@ -53,53 +53,65 @@ def display():
     print(row2)
     print(row3)
 
-    user_input()
+    user_input_row()
 
 
 # Gets the users input
 # Checks validation of input
 # Returns input
-def user_input():
+def user_input_row():
     # Variables
     row = ''
-    column = ''
     acceptable_range = range(1, 4)
     within_range_row = False
+
+    # Two conditions to check for ROW and COLUMN
+    try:
+        while row.isdigit() == False or within_range_row == False:
+            row = input("Please enter a ROW number (1-3): ")
+            # Digit check
+            if row.isdigit() ==  False:
+                print("Sorry, that's not a digit!")
+            elif row.isdigit() == True:
+                if int(row) in acceptable_range:
+                    within_range_row = True
+                else:
+                    print("Sorry, that's not within 1-3")
+        row_choice = int(row)
+        print(f"Row Choice: {row_choice}")
+        return row_choice
+    finally:
+        user_input_column()
+
+
+def user_input_column():
+    # Variables
+    column = ''
+    acceptable_range = range(1, 4)
     within_range_column = False
 
-    # Two conditions to check for ROW
-    # TODO Maybe have to reconfigure these while loops into nested while loops...
-    while row.isdigit() == False or within_range_row == False:
-        row = input("Please enter a ROW number (1-3): ")
-        # Digit check
-        if row.isdigit() == False:
-            print("Sorry, that's not a digit!")
-        # Range check
-        elif row.isdigit() == True:
-            if int(row) in acceptable_range:
-                within_range_row = True
-            else:
-                print("Sorry, that's not within 1-4")
-
     # Two conditions to check for COLUMN
-    while column.isdigit() == False or within_range_column == False:
-        column = input("Please enter a COLUMN number (1-3): ")
-        # Digit Check - Is this a digit?
-        if column.isdigit() == False:
-            print("Sorry, that's not a digit")
-        # Range Check - Is this Digit within Range?
-        if column.isdigit() == True:
-            if int(column) in acceptable_range:
-                within_range_column = True
-            else:
-                print("Sorry, that's not within 1-3")
-    row_choice = int(row)
-    column_choice = int(column)
-    return row_choice, column_choice
+    try:
+        while column.isdigit() == False or within_range_column == False:
+            column = input("Please enter a COLUMN number (1-3): ")
+            # Digit check
+            if column.isdigit() == False:
+                print("Sorry, that's not a digit!")
+            # Range check
+            elif column.isdigit() == True:
+                if int(column) in acceptable_range:
+                    within_range_column = True
+                else:
+                    print("Sorry, that's not within 1-3")
+        column_choice = int(column)
+        print(column_choice)
+        return column_choice
+    finally:
+        translate_user_choice()
 
 
 # Function to change input into correct array index, in prep for replace_choice()
-def translate_user_choice(user_choice):
+def translate_user_choice():
     pass
     # user_choice = list(user_input() + 1)
     # user_choice_translated = user_choice+1
@@ -119,7 +131,6 @@ def check_win():
 
 def main_game():
     welcome_screen()
-    print("After the user input function")
 
 
 main_game()
