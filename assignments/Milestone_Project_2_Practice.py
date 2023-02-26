11faca48b473fd024038f317da107663b3756c80
Chris <chris.cosnowski@gmail.com>
2023-02-25 22:52:23 -0500

    LeetCode practice

--- assignments/Milestone_Project_2_Practice.py
+++ assignments/Milestone_Project_2_Practice.py
@@ -10,7 +10,7 @@ values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8,
           'Nine':9, 'Ten':10, 'Jack':11, 'Queen':12, 'King':13, 'Ace':14}
 
 
-#Testing
+# Testing
 class Card():
 
     def __init__(self,suit,rank):
